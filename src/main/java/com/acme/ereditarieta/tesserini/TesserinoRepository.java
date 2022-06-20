b9abece4f34418d86aa7f683d4a8b602d71b191a
samue <samue@192.168.1.234>
2022-06-20 16:59:00 +0200

    Fixed Tesserini

--- src/main/java/com/acme/ereditarieta/tesserini/TesserinoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.acme.ereditarieta.tesserini;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface TesserinoRepository extends PagingAndSortingRepository<TesserinoAbstract, Long> {
-
-}
