5edb6c1d273b050838f2621a101353751d6e5404
arist <arist@ARISTIDE.homenet.telecomitalia.it>
2022-06-20 16:45:34 +0200

    Commit #2 - Aristide

---
+++ src/main/java/com/acme/ereditarieta/licenze/caccia/LicenzaCacciaController.java
@@ -0,0 +1,67 @@
+package com.acme.ereditarieta.licenze.caccia;
+
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.acme.ereditarieta.licenze.Licenza;
+import com.acme.ereditarieta.licenze.pesca.LicenzaPescaRepository;
+
+@RestController
+@RequestMapping("/licenze")
+public class LicenzaCacciaController {
+	@Autowired
+	LicenzaCacciaRepository licenzaCacciaRepo;
+
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getLicenza (@PathVariable Long id) {
+		if(licenzaCacciaRepo.existsById(id)) {
+			LicenzaCaccia licenzaCaccia = licenzaCacciaRepo.findById(id).get();
+			
+			return new ResponseEntity(licenzaCaccia, HttpStatus.OK);
+			}	else {
+				return new ResponseEntity("Licenza " + id +  " non trovata ", HttpStatus.NOT_FOUND);
+			}
+	}
+	@PostMapping
+	public ResponseEntity<String> insertLicenzaCaccia( @RequestBody Licenza licenza, String tipoLicenza) {
+		if(licenzaCacciaRepo.existsById(licenza.getId())) {
+			return new ResponseEntity<String>("Licenza già esistente", HttpStatus.NOT_ACCEPTABLE);
+			
+		} else {
+			    	licenzaCacciaRepo.save((LicenzaCaccia) licenza);
+			    	return ResponseEntity.ok("Licenza inserita ");
+		}
+	}
+	
+@PutMapping("/{id}")
+	
+	public ResponseEntity<String> updateLicenzaCaccia (@RequestBody Licenza licenza, @PathVariable Long id) {
+		if(licenzaCacciaRepo.existsById(id)) {
+			LicenzaCaccia licenzaInDb = licenzaCacciaRepo.findById(id).get();
+			BeanUtils.copyProperties(licenza, licenzaInDb);
+			licenzaCacciaRepo.save(licenzaInDb);
+			
+			return new ResponseEntity<String> ("Licenza " + id + " modificata", HttpStatus.OK);
+		} else {
+			return new ResponseEntity<String>("Licenza " + id + " non trovata", HttpStatus.NOT_FOUND);
+		}
+	}
+
+@GetMapping("/licenze")
+
+public ResponseEntity <List <LicenzaCaccia>> getTutteLeLicenzeCaccia () {
+	return ResponseEntity.ok((List<LicenzaCaccia>) licenzaCacciaRepo.findAll());
+}
+}
---
+++ src/main/java/com/acme/ereditarieta/licenze/pesca/LicenzaPescaController.java
@@ -0,0 +1,67 @@
+package com.acme.ereditarieta.licenze.pesca;
+
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.acme.ereditarieta.licenze.Licenza;
+import com.acme.ereditarieta.licenze.pesca.LicenzaPescaRepository;
+
+@RestController
+@RequestMapping("/licenze")
+public class LicenzaPescaController {
+	@Autowired
+	LicenzaPescaRepository licenzaPescaRepo;
+
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getLicenza (@PathVariable Long id) {
+		if(licenzaPescaRepo.existsById(id)) {
+			LicenzaPesca licenzaPesca = licenzaPescaRepo.findById(id).get();
+			
+			return new ResponseEntity(licenzaPesca, HttpStatus.OK);
+			}	else {
+				return new ResponseEntity("Licenza " + id +  " non trovata ", HttpStatus.NOT_FOUND);
+			}
+	}
+	@PostMapping
+	public ResponseEntity<String> insertLicenzaPesca( @RequestBody Licenza licenza, String tipoLicenza) {
+		if(licenzaPescaRepo.existsById(licenza.getId())) {
+			return new ResponseEntity<String>("Licenza già esistente", HttpStatus.NOT_ACCEPTABLE);
+			
+		} else {
+			    	licenzaPescaRepo.save((LicenzaPesca) licenza);
+			    	return ResponseEntity.ok("Licenza inserita ");
+		}
+	}
+	
+@PutMapping("/{id}")
+	
+	public ResponseEntity<String> updateLicenzaPesca(@RequestBody Licenza licenza, @PathVariable Long id) {
+		if(licenzaPescaRepo.existsById(id)) {
+			LicenzaPesca licenzaInDb = licenzaPescaRepo.findById(id).get();
+			BeanUtils.copyProperties(licenza, licenzaInDb);
+			licenzaPescaRepo.save(licenzaInDb);
+			
+			return new ResponseEntity<String> ("Licenza " + id + " modificata", HttpStatus.OK);
+		} else {
+			return new ResponseEntity<String>("Licenza " + id + " non trovata", HttpStatus.NOT_FOUND);
+		}
+	}
+
+@GetMapping("/licenze")
+
+public ResponseEntity <List <LicenzaPesca>> getTutteLeLicenzeCaccia () {
+	return ResponseEntity.ok((List<LicenzaPesca>) licenzaPescaRepo.findAll());
+}
+}
