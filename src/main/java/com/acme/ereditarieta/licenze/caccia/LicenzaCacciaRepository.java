0b5c45e040a1c6d89ffe646ee14487aa4643d6fd
arist <arist@ARISTIDE.homenet.telecomitalia.it>
2022-06-20 16:24:54 +0200

    Commit #Aristide

--- src/main/java/com/acme/ereditarieta/licenze/caccia/LicenzaCacciaRepository.java
+++ src/main/java/com/acme/ereditarieta/licenze/caccia/LicenzaCacciaRepository.java
@@ -2,6 +2,9 @@ package com.acme.ereditarieta.licenze.caccia;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import com.acme.ereditarieta.licenze.Licenza;
+
 public interface LicenzaCacciaRepository extends PagingAndSortingRepository<LicenzaCaccia, Long> {
+
 
 }
